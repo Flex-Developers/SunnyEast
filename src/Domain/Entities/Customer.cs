3dc4f52a8ca12954b16cb9e0de44a17200d89a2f
firuz2006 <firuzisoboev@gmail.com>
2023-11-04 19:47:53 +0500

    initial

---
+++ src/Domain/Entities/Customer.cs
@@ -0,0 +1,24 @@
+namespace Domain.Entities;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string LName { get; set; }
+    public string Patronymic { get; set; }
+    public string Phone { get; set; }
+
+    public int ProductsPurchasedCount { get; set; }
+    public decimal ProductsPurchasedSum { get; set; }
+
+    public int SellProductsPurchasedCount { get; set; }
+    public decimal SellProductsPurchasedSum { get; set; }
+
+    public int DiscountProductsPurchasedCount { get; set; }
+    public decimal DiscountProductsPurchasedSum { get; set; }
+
+    public int PurchasedTotalCount { get; set; }
+    public decimal PurchasedTotalSum { get; set; }
+
+    public decimal TotalIncome { get; set; }
+}
