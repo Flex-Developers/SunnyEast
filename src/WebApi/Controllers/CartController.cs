41a5eb55f7948b9342c0ae74799a8040e8aa35e9
Avazbek Olimov <84025676+Avazbek22@users.noreply.github.com>
2024-02-05 18:02:29 +0300

    CartController

--- src/WebApi/Controllers/CartController.cs
+++ src/WebApi/Controllers/CartController.cs
@@ -1,9 +1,12 @@
 ﻿using Application.Contract.Cart.Commands;
+using Application.Contract.Cart.Queries;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using WebApi.Controllers;
 
 namespace WebApi;
 
+[Authorize]
 public class CartController : ApiControllerBase
 {
     [HttpPost]
@@ -19,4 +22,19 @@ public class CartController : ApiControllerBase
         var response = await Mediator.Send(command);
         return Ok(response);
     }
+
+    [HttpGet(nameof(GetCart))]
+    public async Task<IActionResult> GetCart([FromQuery] GetCartQuery query)
+    {
+        var response = await Mediator.Send(query);
+        return Ok(response);
+    }
+
+    [HttpGet(nameof(GetCarts))]
+    public async Task<IActionResult> GetCarts([FromQuery] GetCartsQuery query)
+    {
+        var response = await Mediator.Send(query);
+        return Ok(response);
+    }
+    
 }
