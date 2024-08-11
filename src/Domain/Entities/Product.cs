6290eab89c072c2112a4d6e9c2d38074bdec9068
Avazbek Olimov <avazbekolimov722@gmail.com>
2024-08-11 21:35:50 +0300

    Product price fixed, it changed to decimal(10,2). Added migration ModifiedProductPrice.cs

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Domain.Entities;
 
 public class Product : BaseEntity
@@ -8,7 +10,7 @@ public class Product : BaseEntity
     public required string ProductCategorySlug { get; set; }
     public ProductCategory? ProductCategory { get; set; }
     public string? Description { get; set; }
-
+    [Column(TypeName = "decimal(10,2)")]
     public required decimal Price { get; set; }
     public string[]? Images { get; set; }
 }
