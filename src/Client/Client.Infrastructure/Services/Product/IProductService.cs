85b22197ff551f688b4769ab0131d4a0a5e9a379
Avazbek Olimov <avazbekolimov722@gmail.com>
2024-08-14 13:54:23 +0300

    Added GetProductsByCategoryNameQuery.cs, GetProductsByCategoryNameQueryHandler.cs

--- src/Client/Client.Infrastructure/Services/Product/IProductService.cs
+++ src/Client/Client.Infrastructure/Services/Product/IProductService.cs
@@ -10,5 +10,6 @@ public interface IProductService
     public Task<bool> Put(UpdateProductCommand command);
     public Task<ProductResponse?> Get(string slug);
     public Task<List<ProductResponse>> Get(GetProductsQuery query);
+    public Task<List<ProductResponse>> GetByCategoryName(string categoryName);
     public Task<bool> Delete(string slug);
 }
