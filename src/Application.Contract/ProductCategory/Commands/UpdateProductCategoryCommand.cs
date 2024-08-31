193cebce60b2b10a79071baeae541d46838197b8
Avazbek Olimov <avazbekolimov722@gmail.com>
2024-08-31 10:38:24 +0300

    Added Discount property to models and entities. Also added migration

--- src/Application.Contract/ProductCategory/Commands/UpdateProductCategoryCommand.cs
+++ src/Application.Contract/ProductCategory/Commands/UpdateProductCategoryCommand.cs
@@ -5,4 +5,5 @@ public class UpdateProductCategoryCommand : IRequest<string>
     public required string Slug { get; set; }
     public required string Name { get; set; }
     public string? BaseCategorySlug { get; set; }
+    public byte? DiscountPercentage { get; set; }
 }
--- src/Domain/Entities/ProductCategory.cs
+++ src/Domain/Entities/ProductCategory.cs
@@ -4,7 +4,7 @@ public class ProductCategory : BaseEntity
 {
     public required string Name { get; set; }
     public required string Slug { get; set; }
-
+    public byte? DiscountPercentage { get; set; }
     public Guid? BaseProductCategoryId { get; set; }
     public ProductCategory? BaseProductCategory { get; set; }
 }
