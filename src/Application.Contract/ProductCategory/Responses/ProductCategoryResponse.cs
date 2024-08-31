cfe79dc5cc483b556c38449c62fc61eb06b5d2d8
Avazbek Olimov <avazbekolimov722@gmail.com>
2024-08-31 11:00:43 +0300

    Discount props added to responses

--- src/Application.Contract/ProductCategory/Responses/ProductCategoryResponse.cs
+++ src/Application.Contract/ProductCategory/Responses/ProductCategoryResponse.cs
@@ -4,5 +4,6 @@ public class ProductCategoryResponse
 {
     public required string Slug { get; set; }
     public required string Name { get; set; }
+    public byte? DiscountPercentage { get; set; }
     public string? BaseCategorySlug { get; set; }
 }
