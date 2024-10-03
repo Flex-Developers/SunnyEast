21d8ff9bb5535ac9b1953c283bcff2d1b20464d5
Avazbek Olimov <avazbekolimov722@gmail.com>
2024-10-03 17:37:18 +0300

    Added Images array to Shop entities

---
+++ src/Infrastructure/Migrations/20241003090649_Shops_Added_Images_Array.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Shops_Added_Images_Array : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Shops",
+                newName: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Images",
+                table: "Shops",
+                newName: "ImageUrl");
+        }
+    }
+}
