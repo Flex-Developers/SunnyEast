9a5a598ca2222210bf6ded9971b208e792a31ead
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-04-02 18:46:33 +0300

    ProductVolume implemented on Category

--- src/Application.Contract/ProductCategory/Commands/CreateProductCategoryCommand.cs
+++ src/Application.Contract/ProductCategory/Commands/CreateProductCategoryCommand.cs
@@ -2,8 +2,9 @@ namespace Application.Contract.ProductCategory.Commands;
 
 public class CreateProductCategoryCommand : IRequest<string>
 {
-    public required string Name { get; set; }
     public string? BaseCategorySlug { get; set; }
+    public required string Name { get; set; }
     public byte? DiscountPercentage { get; set; }
     public string? ImageUrl { get; set; }
+    public string[]? ProductVolumes { get; set; }
 }
---
+++ src/Infrastructure/Migrations/20250402144827_Remove_Unused_Column.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Unused_Column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductVolume",
+                table: "ProductCategories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
