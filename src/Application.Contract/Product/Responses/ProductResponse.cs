8f26c06ab7159ec4a72881580cce720ed7df8b86
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-04-15 14:00:02 +0300

    ProductResponse.cs changed

--- src/Application.Contract/Product/Responses/ProductResponse.cs
+++ src/Application.Contract/Product/Responses/ProductResponse.cs
@@ -3,6 +3,7 @@ namespace Application.Contract.Product.Responses;
 public record ProductResponse
 {
     public required string Slug { get; set; }
+    public required string ProductCategorySlug { get; set; }
     public required string LevelSlug { get; set; }
     public string Name { get; set; } = "";
     public required decimal Price { get; set; }
@@ -11,5 +12,6 @@ public record ProductResponse
     public string[]? Images { get; set; }
     public string? Description { get; set; }
     public string? SelectedVolume { get; set; } // maybe temporary
+    public string[]? ProductVolumes { get; set; }
 
 }
