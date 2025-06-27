267ee5e262c2774dfc8fbd2983a05e348b91867f
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-06-27 19:40:30 +0300

    LevelSlug removed from ProductResponse.cs

--- src/Application.Contract/Product/Responses/ProductResponse.cs
+++ src/Application.Contract/Product/Responses/ProductResponse.cs
@@ -6,7 +6,6 @@ public record ProductResponse
 {
     public required string Slug { get; set; }
     public required string ProductCategorySlug { get; set; }
-    public required string LevelSlug { get; set; }
     public string Name { get; set; } = "";
     public required decimal Price { get; set; }
     public decimal? DiscountPrice { get; set; }
