ee673dcf79b5154d6f623825475f57468fbe8474
firuz2006 <firuzisoboev@gmail.com>
2025-07-21 11:21:59 +0500

    Refactor AppBar and MainLayout components to simplify cart display and streamline theme preference handling

--- src/Client/Client/Layout/MainLayout.razor.cs
+++ src/Client/Client/Layout/MainLayout.razor.cs
@@ -15,21 +15,9 @@ public partial class MainLayout
     [Parameter] public EventCallback OnDarkModeToggle { get; set; }
     private bool _drawerOpen;
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
-            StateHasChanged();
-        }
-    }
-
     protected override async Task OnInitializedAsync()
     {
-        _themePreference = await ClientPreferences.GetPreference() as ClientPreference;
-
-        if (_themePreference == null)
-            _themePreference = new ClientPreference();
+        _themePreference = await ClientPreferences.GetPreference() as ClientPreference ?? new ClientPreference();
 
         SetCurrentTheme(_themePreference);
     }
@@ -38,18 +26,6 @@ public partial class MainLayout
     {
         _isDarkMode = themePreference.IsDarkMode;
         _drawerOpen = themePreference.IsDrawerOpen;
-        StateHasChanged();
-    }
-
-    private async Task ToggleDarkMode()
-    {
-        _isDarkMode = !_isDarkMode;
-        if (_themePreference != null)
-        {
-            _themePreference.IsDarkMode = _isDarkMode;
-            await ClientPreferences.SetPreference(_themePreference);
-        }
-        await OnDarkModeToggle.InvokeAsync();
         StateHasChanged();
     }
 }
