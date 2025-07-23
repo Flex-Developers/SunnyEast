76c519e29d3386276a86cbf3bc0b23d9bcaacdc0
firuz2006 <firuzisoboev@gmail.com>
2025-07-24 00:05:15 +0500

    Refactor order command handlers to improve user validation and enhance exception handling

--- src/Application/Features/Orders/Commands/CancelOrderCommandHandler.cs
+++ src/Application/Features/Orders/Commands/CancelOrderCommandHandler.cs
@@ -1,7 +1,6 @@
 using Application.Common.Exceptions;
 using Application.Common.Interfaces.Contexts;
 using Application.Common.Interfaces.Services;
-using Application.Contract.Identity;
 using Application.Contract.Order.Commands;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -17,22 +16,23 @@ public sealed class CancelOrderCommandHandler(
     {
         var order = await context.Orders
                         .Include(o => o.OrderItems)
+                        .Include(f => f.Customer)
                         .FirstOrDefaultAsync(o => o.Slug == req.Slug, ct)
                     ?? throw new NotFoundException($"Заказ {req.Slug} не найден.");
 
-       
-        //TODO: Fix exception here from GetUserID()....
-        if (order.CustomerId != currentUserService.GetUserId() && currentUserService.GetUserName() is not "Administrator" &&
-            currentUserService.GetUserName() is not "Salesman")
-        {
+
+        var userName = currentUserService.GetUserName() ?? throw new ForbiddenException();
+
+        if (!userName.Equals(order.Customer?.UserName, StringComparison.CurrentCultureIgnoreCase)
+            && userName is not "Administrator" && userName is not "Salesman")
             throw new ForbiddenException();
-        }
 
 
-        if (order.Status is not (Domain.Enums.OrderStatus.Submitted or Domain.Enums.OrderStatus.InProgress or Domain.Enums.OrderStatus.Ready))
+        if (order.Status is not (Domain.Enums.OrderStatus.Submitted or Domain.Enums.OrderStatus.InProgress
+            or Domain.Enums.OrderStatus.Ready))
             throw new BadRequestException("Этот заказ уже нельзя отменить.");
 
-        
+
         order.Status = Domain.Enums.OrderStatus.Canceled;
         order.CanceledAt = DateTime.UtcNow;
 
